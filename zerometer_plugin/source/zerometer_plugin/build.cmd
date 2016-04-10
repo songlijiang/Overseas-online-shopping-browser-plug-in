5807786ae481312ae8ef91b55183011a66db3b24
slj <lijiang.song@ipiaoniu.com>
2016-04-10 11:44:50 +0800

    1.0.4

--- zerometer_plugin/source/zerometer_plugin/build.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-<<<<<<< .mine
-SET KANGODIR="F:\cango\kango-framework-latest"
-=======
-SET KANGODIR=E:\work\ExtensionDevelop\kango-framework-latest
->>>>>>> .r6877
-
-call "%KANGODIR%\kango.py" build .\
